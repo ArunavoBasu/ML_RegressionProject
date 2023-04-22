4aa40cd6e233d30fdab751295482f60b813f098a
unknown <arunavobasu.ds@gmail.com>
2023-04-22 13:01:01 +0530

    created api in flask, taken input from user, model prediction from the inputs given by user

--- src/utils.py
+++ src/utils.py
@@ -50,3 +50,14 @@ def evaluate_model(X_train, y_train, X_test, y_test, models):
     except Exception as e:
         logging("Error occured during model training")
         raise CustomException(e,sys)
+
+
+#------------ To Load the pickle file ------------------
+    
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        logging.info('Exception Occured in load_object function utils')
+        raise CustomException(e,sys)
